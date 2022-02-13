508ece4d911fbfd376ed7f41939fe5b53242fa92
Anri <anri0806@hotmail.co.jp>
2022-02-13 11:41:10 -0500

    commit tree 04bc04264a1444b1236b7e4a91e6467e58919f51

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+#1 create class Anagram
+#2 create initialize (word)
+#3 create #match (arr) instance method
+#  find and return if @word matches with parameter
+#  arr.each {|a| 
+#      a.chars to get individual letters
+#      arr.sort == newArr.sort to compare if they match
+
+require 'pry'
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        arr.filter {|ar| ar.chars.sort == @word.chars.sort ? ar : nil}
+    end
+
+end
